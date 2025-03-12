56142d9b3fcb18b06b85d9765790d19af7dba496
Roshni Gupta <roshni.g@turing.com>
2025-03-12 11:31:10 +0530

    Initialize an Empty Transposed Matrix

--- task1.py
+++ task1.py
@@ -2,4 +2,7 @@ def transpose_matrix(matrix):
     """function to transponse a given matrix."""
     rows = len(matrix) #Number of rows
     cols = len(matrix[0]) if rows > 0 else 0 #Number of columns
+
+    # Initialize the transposed matrix with empty lists
+    transposed = [[0] * rows for _ in range(cols)]  # Creating cols x rows matrix
     
